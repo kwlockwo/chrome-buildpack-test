2ad69d559bce88e554f650cac22be2fedec3e4f2
Keith Lockwood <kwlockwo@gmail.com>
2017-08-24 09:28:26 +1000

    Add Testing

--- src/test/java/com/heroku/ChromeTest.java
+++ src/test/java/com/heroku/ChromeTest.java
@@ -1,9 +1,12 @@
 package com.heroku;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.tap4j.ext.junit.runner.TapRunnerClass;
 
+@RunWith(TapRunnerClass.class)
 public class ChromeTest {
 
 	@Test
