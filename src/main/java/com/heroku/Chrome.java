86cd2826a51424b9051aa107e3f28a02bbfee3fb
Keith Lockwood <kwlockwo@gmail.com>
2019-03-29 01:03:29 +1100

    Update Chrome.java

--- src/main/java/com/heroku/Chrome.java
+++ src/main/java/com/heroku/Chrome.java
@@ -2,6 +2,7 @@ package com.heroku;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
